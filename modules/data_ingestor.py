e744cc9952f176c28b0f0da8c36a792db6e5dec3
bhavikachopra06 <bhavikachopra67@gmail.com>
2026-02-21 19:38:15 +0530

    Phase 3 Done 🚀

--- modules/data_ingestor.py
+++ modules/data_ingestor.py
@@ -1,9 +1,6 @@
 import pandas as pd
-# import numpy as np
 import matplotlib.pyplot as plt
-# import matplotlib.dates as mdates
 import os
-# import sys
 from config import (
     REQUIRED_COLUMNS, TIMESTAMP_CANDIDATES,
     MAX_SINGLE_BAR_MOVE_PCT, MIN_PRICE,
